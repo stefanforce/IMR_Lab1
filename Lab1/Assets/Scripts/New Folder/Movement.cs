874e530789fbf22bb2f3e91b3b65476d09289a60
unknown <stefantiperciuc@yahoo.com>
2020-10-07 10:23:08 +0300

    added files

---
+++ Lab1/Assets/Scripts/New Folder/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cube;
+    private Transform cubeTransform;
+    private float zPos = 0;
+
+    void Start()
+    {
+        cubeTransform = cube.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeTransform.position = new Vector3(0, 0, zPos);
+        zPos = zPos + 0.0001f;
+    }
+}
